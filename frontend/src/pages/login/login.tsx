8ef5039cba454d3fe48c89f0e6dc917abc2e929a
Mateusz Sikora <ms1qaz@gmail.com>
2023-05-10 11:50:31 +0200

    working indieauth

--- frontend/src/pages/login/login.tsx
+++ frontend/src/pages/login/login.tsx
@@ -1,3 +1,19 @@
+import styles from './styles.module.scss';
+
 export const Login = () => {
-  return <div>Login</div>
+  const origin = window.location.origin
+  const state = Math.random().toString().substring(2, 8); 
+
+  return (
+    <div className={styles.bgImg}>
+    <form action="https://indieauth.com/auth" method="get" className={styles.container}>
+      <label htmlFor="url">Web Address:</label>
+      <input id="url" type="text" name="me" placeholder="yourdomain.com" />
+      <button type="submit" className={styles.btn}>Login</button>
+      <input type="hidden" name="client_id" value={`${origin}`} />
+      <input type="hidden" name="redirect_uri" value={`${origin}`} />
+      <input type="hidden" name="state" value={state} />
+    </form>
+  </div>
+  )
 }
