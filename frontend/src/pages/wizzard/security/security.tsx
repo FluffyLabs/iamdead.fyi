3e71590c1ecb9a620a46aa727daa03c918ac1b53
Mateusz Sikora <ms1qaz@gmail.com>
2023-05-26 09:25:49 +0200

    refactor

--- frontend/src/pages/wizzard/security/security.tsx
+++ frontend/src/pages/wizzard/security/security.tsx
@@ -1,28 +1,50 @@
 import { ReactComponent as KeyPerson } from './icons/key-person.svg';
 import { ReactComponent as Key } from './icons/key.svg';
+import { CSSProperties, ComponentType, SVGProps } from 'react';
+
+const MULTIPLICATION_CHAR = '\u{215}';
 
 export const Security = () => {
+  const data = {
+    noOfRecipients: 4,
+    noOfAdditionalPieces: 1,
+  };
   return (
     <div>
-      <div className="flex flex-row items-center">
-        <span className="flex flex-row w-40 justify-around">
-          <KeyPerson style={{ width: '100px', height: '100px' }} />
-          <span style={{ fontSize: '50px', marginLeft: '5px' }}>&#215;4</span>
-        </span>
-        <p className="mx-5">
-          I want any 4 recipients to come together to read the message
-        </p>
-      </div>
+      <Row
+        Icon={KeyPerson}
+        counter={`${MULTIPLICATION_CHAR}${data.noOfRecipients}`}
+        text={`I want any ${data.noOfRecipients} recipients to come together to read the message`}
+      />
 
-      <div className="flex flex-raw items-center">
-        <span className="flex flex-row w-40 justify-between">
-          <Key style={{ width: '100px', height: '100px' }} />
-          <span style={{ fontSize: '50px', marginLeft: '5px' }}>+1</span>
-        </span>
-        <p className="mx-5">
-          For redundancy I want 5 pieces to be distributed
-        </p>
-      </div>
+      <Row
+        Icon={Key}
+        counter={`+${data.noOfAdditionalPieces}`}
+        text={`For redundancy I want ${data.noOfAdditionalPieces} pieces to be distributed`}
+      />
+    </div>
+  );
+};
+
+
+const Row = ({
+  Icon,
+  counter,
+  text,
+}: {
+  Icon: ComponentType<SVGProps<SVGSVGElement>>;
+  counter: string;
+  text: string;
+}) => {
+  return (
+    <div className="flex flex-row items-center">
+      <span className="flex flex-row w-40 justify-around">
+        <Icon style={{ width: '100px', height: '100px' }} />
+        <span style={{ fontSize: '50px', marginLeft: '5px' }}>{counter}</span>
+      </span>
+      <p className="mx-5">
+        {text}
+      </p>
     </div>
   );
 };
