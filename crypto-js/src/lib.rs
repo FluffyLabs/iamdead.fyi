aa1b26bdf9edea5ffd572ba536f9e838629d5ac6
Tomasz Drwięga <tomusdrw@gmail.com>
2023-06-13 16:51:50 +0100

    fmt & workflow

--- crypto-js/src/lib.rs
+++ crypto-js/src/lib.rs
@@ -1,70 +1,72 @@
-use icod_crypto::encryption::{MessageEncryptionKey, Message};
+use icod_crypto::encryption::{Message, MessageEncryptionKey};
 use serde::Serialize;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 #[derive(Debug)]
 pub enum Error {
-    InvalidKeySize,
-    VersionError,
-    CryptoError,
+  InvalidKeySize,
+  VersionError,
+  CryptoError,
 }
 
 impl From<Error> for JsValue {
-    fn from(value: Error) -> Self {
-        JsValue::from_str(&format!("{:?}", value))
-    }
+  fn from(value: Error) -> Self {
+    JsValue::from_str(&format!("{:?}", value))
+  }
 }
 
 impl From<icod_crypto::encryption::Error> for Error {
-    fn from(value: icod_crypto::encryption::Error) -> Self {
-        match value {
-            icod_crypto::encryption::Error::UnsupportedVersion => Self::VersionError,
-            icod_crypto::encryption::Error::EncryptionError => Self::CryptoError,
-        }
+  fn from(value: icod_crypto::encryption::Error) -> Self {
+    match value {
+      icod_crypto::encryption::Error::UnsupportedVersion => Self::VersionError,
+      icod_crypto::encryption::Error::EncryptionError => Self::CryptoError,
     }
+  }
 }
 
 #[derive(Serialize)]
 pub struct EncryptedMessage {
-    pub data: Vec<u8>,
-    pub nonce: Vec<u8>,
+  pub data: Vec<u8>,
+  pub nonce: Vec<u8>,
 }
 
-
 fn parse_key(key: Vec<u8>) -> Result<[u8; 32], Error> {
-    let mut out = [0u8; 32];
-    if key.len() != 32 {
-        return Err(Error::InvalidKeySize)
-    }
-    out.copy_from_slice(&key);
-    Ok(out)
+  let mut out = [0u8; 32];
+  if key.len() != 32 {
+    return Err(Error::InvalidKeySize);
+  }
+  out.copy_from_slice(&key);
+  Ok(out)
 }
 
 #[wasm_bindgen]
 pub fn encrypt_message(key: Vec<u8>, msg: String) -> Result<JsValue, Error> {
-    let key = parse_key(key)?;
-    let key = MessageEncryptionKey::new(key);
-    let msg = Message::from_str(&msg);
+  let key = parse_key(key)?;
+  let key = MessageEncryptionKey::new(key);
+  let msg = Message::from_str(&msg);
 
-    let encrypted = icod_crypto::encryption::encrypt_message(&key, &msg)?;
+  let encrypted = icod_crypto::encryption::encrypt_message(&key, &msg)?;
 
-    let (data, nonce) = encrypted.into_tuple();
+  let (data, nonce) = encrypted.into_tuple();
 
-    Ok(serde_wasm_bindgen::to_value(&EncryptedMessage {
-        data: data.into(),
-        nonce: nonce.into(),
-    }).expect("EncryptedMessage serialization is infallible"))
+  Ok(
+    serde_wasm_bindgen::to_value(&EncryptedMessage {
+      data: data.into(),
+      nonce: nonce.into(),
+    })
+    .expect("EncryptedMessage serialization is infallible"),
+  )
 }
 
 #[wasm_bindgen]
 pub fn decrypt_message(key: Vec<u8>, data: Vec<u8>, nonce: Vec<u8>) -> Result<Vec<u8>, Error> {
-    let key = parse_key(key)?;
-    let key = MessageEncryptionKey::new(key);
-    let msg = icod_crypto::encryption::EncryptedMessage::new(data, nonce);
+  let key = parse_key(key)?;
+  let key = MessageEncryptionKey::new(key);
+  let msg = icod_crypto::encryption::EncryptedMessage::new(data, nonce);
 
-    let decrypted = icod_crypto::encryption::decrypt_message(&key, &msg)?;
+  let decrypted = icod_crypto::encryption::decrypt_message(&key, &msg)?;
 
-    let (data, _nonce) = decrypted.into_tuple();
-    Ok(data.into())
+  let (data, _nonce) = decrypted.into_tuple();
+  Ok(data.into())
 }
--- crypto/src/lib.rs
+++ crypto/src/lib.rs
@@ -15,58 +15,60 @@ pub mod encryption;
 /// A representation of 32-bytes secure cryptographic hash output.
 #[derive(PartialEq, Eq)]
 pub struct Hash {
-    data: [u8; 32],
+  data: [u8; 32],
 }
 
 impl std::fmt::Debug for Hash {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_tuple("Hash")
-            .field(&hex::encode(&self.data))
-            .finish()
-    }
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    f.debug_tuple("Hash")
+      .field(&hex::encode(&self.data))
+      .finish()
+  }
 }
 
 /// A raw vector of arbitrary-length bytes with additional formatting.
 #[derive(Clone, PartialEq, Eq)]
 pub struct Bytes {
-    data: Vec<u8>,
+  data: Vec<u8>,
 }
 
 impl Bytes {
-    /// Create a new [Bytes] struct by copying bytes from the slice.
-    pub fn from_slice(bytes: &[u8]) -> Self {
-        Self { data: bytes.to_vec() }
+  /// Create a new [Bytes] struct by copying bytes from the slice.
+  pub fn from_slice(bytes: &[u8]) -> Self {
+    Self {
+      data: bytes.to_vec(),
     }
+  }
 }
 
 impl Deref for Bytes {
-    type Target = [u8];
+  type Target = [u8];
 
-    fn deref(&self) -> &Self::Target {
-        &*self.data
-    }
+  fn deref(&self) -> &Self::Target {
+    &*self.data
+  }
 }
 
 impl From<Vec<u8>> for Bytes {
-    fn from(data: Vec<u8>) -> Self {
-        Self { data }
-    }
+  fn from(data: Vec<u8>) -> Self {
+    Self { data }
+  }
 }
 
 impl From<Bytes> for Vec<u8> {
-    fn from(value: Bytes) -> Self {
-        value.data
-    }
+  fn from(value: Bytes) -> Self {
+    value.data
+  }
 }
 
 impl std::fmt::Debug for Bytes {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let hex = hex::encode(&self.data);
-        match std::str::from_utf8(&self.data) {
-            Ok(s) => write!(f, "String({:?}) == Bytes({:?})", s, hex),
-            Err(_) => write!(f, "Bytes({:?})", hex),
-        }
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    let hex = hex::encode(&self.data);
+    match std::str::from_utf8(&self.data) {
+      Ok(s) => write!(f, "String({:?}) == Bytes({:?})", s, hex),
+      Err(_) => write!(f, "Bytes({:?})", hex),
     }
+  }
 }
 
 /*
@@ -94,22 +96,24 @@ pub fn restore_key(_chunks: Vec<Chunk>) -> Result<MessageEncryptionKey, ()> {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+  use super::*;
 
-    #[test]
-    fn should_format_hash() {
-        let hash = Hash { data: [0u8; 32] };
-        assert_eq!(
-            &format!("{:?}", hash),"Hash(\"0000000000000000000000000000000000000000000000000000000000000000\")");
-    }
+  #[test]
+  fn should_format_hash() {
+    let hash = Hash { data: [0u8; 32] };
+    assert_eq!(
+      &format!("{:?}", hash),
+      "Hash(\"0000000000000000000000000000000000000000000000000000000000000000\")"
+    );
+  }
 
-    #[test]
-    fn should_format_bytes() {
-        let bytes = Bytes::from_slice(b"these are str bytes");
+  #[test]
+  fn should_format_bytes() {
+    let bytes = Bytes::from_slice(b"these are str bytes");
 
-        assert_eq!(
-            &format!("{:?}", bytes),
-            r#"String("these are str bytes") == Bytes("74686573652061726520737472206279746573")"#,
-        );
-    }
+    assert_eq!(
+      &format!("{:?}", bytes),
+      r#"String("these are str bytes") == Bytes("74686573652061726520737472206279746573")"#,
+    );
+  }
 }
