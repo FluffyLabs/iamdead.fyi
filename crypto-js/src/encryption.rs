17440016f164b04d510dcbac7a1a3b6cfaaf3d53
Tomasz Drwięga <tomusdrw@gmail.com>
2023-06-27 17:35:44 +0200

    Return a proper error in case of nonce length mismatch.

--- crypto-js/src/encryption.rs
+++ crypto-js/src/encryption.rs
@@ -1,4 +1,4 @@
-use icod_crypto::encryption::{Message, MessageEncryptionKey};
+use icod_crypto::encryption::{Message, MessageEncryptionKey, NONCE_SIZE};
 use serde::Serialize;
 use wasm_bindgen::prelude::*;
 
@@ -6,6 +6,7 @@ use wasm_bindgen::prelude::*;
 #[derive(Debug)]
 pub enum Error {
   InvalidKeySize,
+  InvalidNonceSize,
   VersionError,
   CryptoError,
 }
@@ -52,14 +53,16 @@ pub fn encrypt_message(key: Vec<u8>, msg: String) -> Result<JsValue, Error> {
 
 #[wasm_bindgen]
 pub fn decrypt_message(key: Vec<u8>, data: Vec<u8>, nonce: Vec<u8>) -> Result<Vec<u8>, Error> {
-  let key = crate::parse_key(key).map_err(|_| Error::InvalidKeySize)?;
-  let key = MessageEncryptionKey::new(key);
   let nonce = {
-    let mut n = [0u8; 12];
-    // TODO [ToDr] don't panic here!!!
+    let mut n = [0u8; NONCE_SIZE];
+    if nonce.len() != NONCE_SIZE {
+        return Err(Error::InvalidNonceSize);
+    }
     n.copy_from_slice(&nonce);
     n
   };
+  let key = crate::parse_key(key).map_err(|_| Error::InvalidKeySize)?;
+  let key = MessageEncryptionKey::new(key);
   let msg = icod_crypto::encryption::EncryptedMessage::new(data, nonce);
 
   let decrypted = icod_crypto::encryption::decrypt_message(&key, &msg)?;
