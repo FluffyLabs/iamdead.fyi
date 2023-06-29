bf360075e6bb9666f9b5df46b3030a8daee6a229
Mateusz Sikora <ms1qaz@gmail.com>
2023-06-29 22:01:25 +0200

    split list into components

--- frontend/src/pages/wizzard/proof-of-life/proof-of-life.tsx
+++ frontend/src/pages/wizzard/proof-of-life/proof-of-life.tsx
@@ -1,30 +1,100 @@
+import { useMemo } from 'react';
+
 import styles from './styles.module.scss';
+
+type ConfiguredAdapter = {
+  adapter: Adapters;
+  time: number;
+  unit: Units;
+};
+
+enum Adapters {
+  Email = 'email',
+  Telegram = 'telegram',
+  Twitter = 'twitter',
+  Whatsapp = 'whatsapp',
+}
+
+const availableAdapters = Object.values(Adapters);
+
+const socialMediaAdapters: typeof availableAdapters = [Adapters.Twitter];
+const messageAdapters: typeof availableAdapters = [
+  Adapters.Email,
+  Adapters.Telegram,
+  Adapters.Whatsapp,
+];
+
+const isMessageAdapter = (adapter : Adapters) =>
+  messageAdapters.includes(adapter);
+const isSocialAdapter = (adapter: Adapters) =>
+  socialMediaAdapters.includes(adapter);
+
+const getText = ({ adapter }: ConfiguredAdapter) => {
+  if (isMessageAdapter(adapter)) {
+    return `I don't respond to ${adapter} for`;
+  }
+
+  if (isSocialAdapter(adapter)) {
+    return `I am not active on ${adapter} for`;
+  }
+
+  return '';
+};
+
+enum Units {
+  Months = 'months',
+}
+
+const createItem = (adapter: Adapters, time: number, unit = Units.Months) => ({
+  adapter,
+  time,
+  unit,
+});
+
+const list = [
+  [createItem(Adapters.Email, 2), createItem(Adapters.Telegram, 2)],
+  [createItem(Adapters.Twitter, 2), createItem(Adapters.Whatsapp, 2)],
+  [createItem(Adapters.Twitter, 2), createItem(Adapters.Email, 2)],
+];
 
 export const ProofOfLife = () => {
   return (
     <div>
       <h2 className={styles.header}>I want the pieces to be send when:</h2>
-      <ul className={styles.mainList}>
-        <li>
-          <ul className={styles.subList}>
-            <li>I don't respond to email for 3 months</li>
-            <li>or I'm not active on twitter for 2 months </li>
-            <li>+ or</li>
-          </ul>
-        </li>
-
-        <li>and</li>
-
-        <li>
-          <ul className={styles.subList}>
-            <li>I don't respond to email for 3 months</li>
-            <li>or I'm not active on twitter for 2 months </li>
-            <li>+ or</li>
-          </ul>
-        </li>
-
-        <li>+ and</li>
-      </ul>
+      <POLList />
     </div>
   );
+};
+
+const POLList = () => {
+  return (
+    <ul className={styles.mainList}>
+      {list.map((listOfAdapters, i) => (
+        <>
+          <POLSubList items={listOfAdapters} key={i} />
+          {i < listOfAdapters.length && <li>and</li>}
+        </>
+      ))}
+      <li>+ and</li>
+    </ul>
+  );
+};
+
+const POLSubList = ({ items }: { items: Array<ConfiguredAdapter> }) => {
+  return (
+    <ul className={styles.subList}>
+      {items.map((adapter, i) => (
+        <POLSubListItme adapter={adapter} key={i} />
+      ))}
+      <li>+ or</li>
+    </ul>
+  );
+};
+
+const POLSubListItme = ({ adapter }: { adapter: ConfiguredAdapter }) => {
+  const text = useMemo(
+    () => `${getText(adapter)} ${adapter.time} ${adapter.unit}`,
+    [adapter],
+  );
+  return <li>{text}</li>;
 };
