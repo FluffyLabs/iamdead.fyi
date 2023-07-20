2461b7202fa0cef639026cc3583a83bce8729cab
Tomasz Drwięga <tomusdrw@gmail.com>
2023-07-20 13:16:28 +0200

    Format.

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -1,17 +1,19 @@
 import CryptoJs from 'icod-crypto-js';
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 
 export const Home = () => {
   const [val, setVal] = useState(null);
 
   useEffect(() => {
     const cryptojs = CryptoJs();
-    cryptojs.then(v => {
-      setVal(v as any);
-      console.log(v);
-    }).catch(e => {
-      console.error(e);
-    });
+    cryptojs
+      .then((v) => {
+        setVal(v as any);
+        console.log(v);
+      })
+      .catch((e) => {
+        console.error(e);
+      });
   });
-  return <div>Home: { Object.keys(val || {}).join(', ') }</div>;
+  return <div>Home: {Object.keys(val || {}).join(', ')}</div>;
 };
