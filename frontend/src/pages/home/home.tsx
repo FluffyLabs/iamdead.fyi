72d8e1f4c4bda4025aa0fec72d8f964c86c8d0bb
Tomasz Drwięga <tomusdrw@users.noreply.github.com>
2023-07-24 14:03:32 +0200

    Example of using the WASM module (#49)
    * Example of using the WASM module
    
    * Forcefully add compiled library.
    
    * Format.
    
    * Re-compile frontend in case pkg/** changes

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -1,3 +1,19 @@
+import CryptoJs from 'icod-crypto-js';
+import { useEffect, useState } from 'react';
+
 export const Home = () => {
-  return <div>Home</div>;
+  const [val, setVal] = useState(null);
+
+  useEffect(() => {
+    const cryptojs = CryptoJs();
+    cryptojs
+      .then((v) => {
+        setVal(v as any);
+        console.log(v);
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  });
+  return <div>Home: {Object.keys(val || {}).join(', ')}</div>;
 };
