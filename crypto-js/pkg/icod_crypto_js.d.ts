bfa414ed73cf903524def811e2fc2cf827e57e31
Tomasz Drwięga <tomusdrw@gmail.com>
2023-07-24 21:52:44 +0200

    Initial version of securing the message.

--- crypto-js/pkg/icod_crypto_js.d.ts
+++ crypto-js/pkg/icod_crypto_js.d.ts
@@ -1,21 +1,104 @@
 /* tslint:disable */
 /* eslint-disable */
 /**
-* @param {string} _key
-* @param {string} _msg
+* @param {Uint8Array} key
+* @param {ChunksConfiguration} configuration
+* @returns {any[]}
+*/
+export function split_into_chunks(key: Uint8Array, configuration: ChunksConfiguration): any[];
+/**
+* @param {any[]} chunks
 * @returns {Uint8Array}
 */
-export function encrypt_message(_key: string, _msg: string): Uint8Array;
+export function recover_key(chunks: any[]): Uint8Array;
+/**
+* @param {string} msg
+* @param {ChunksConfiguration} chunks_configuration
+* @returns {any}
+*/
+export function secure_message(msg: string, chunks_configuration: ChunksConfiguration): any;
+/**
+* @param {Uint8Array} data
+* @param {Uint8Array} nonce
+* @param {any[]} chunks
+* @returns {Uint8Array}
+*/
+export function restore_message(data: Uint8Array, nonce: Uint8Array, chunks: any[]): Uint8Array;
+/**
+* @param {Uint8Array} key
+* @param {string} msg
+* @returns {any}
+*/
+export function encrypt_message(key: Uint8Array, msg: string): any;
+/**
+* @param {Uint8Array} key
+* @param {Uint8Array} data
+* @param {Uint8Array} nonce
+* @returns {Uint8Array}
+*/
+export function decrypt_message(key: Uint8Array, data: Uint8Array, nonce: Uint8Array): Uint8Array;
+/**
+*/
+export enum SplittingError {
+  InvalidKeySize = 0,
+  ConfigurationError = 1,
+}
+/**
+*/
+export enum RecoveryError {
+  ChunkDecodingError = 0,
+  InconsistentChunks = 1,
+  InconsistentConfiguration = 2,
+  NotEnoughChunks = 3,
+  UnexpectedKey = 4,
+  KeyDecodingError = 5,
+}
+/**
+*/
+export enum Error {
+  InvalidKeySize = 0,
+  InvalidNonceSize = 1,
+  VersionError = 2,
+  CryptoError = 3,
+}
+/**
+*/
+export class ChunksConfiguration {
+  free(): void;
+/**
+* @param {number} required
+* @param {number} spare
+*/
+  constructor(required: number, spare: number);
+/**
+*/
+  required: number;
+/**
+*/
+  spare: number;
+}
 
 export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
 
 export interface InitOutput {
   readonly memory: WebAssembly.Memory;
+  readonly __wbg_chunksconfiguration_free: (a: number) => void;
+  readonly __wbg_get_chunksconfiguration_required: (a: number) => number;
+  readonly __wbg_set_chunksconfiguration_required: (a: number, b: number) => void;
+  readonly __wbg_get_chunksconfiguration_spare: (a: number) => number;
+  readonly __wbg_set_chunksconfiguration_spare: (a: number, b: number) => void;
+  readonly chunksconfiguration_new: (a: number, b: number) => number;
+  readonly split_into_chunks: (a: number, b: number, c: number, d: number) => void;
+  readonly recover_key: (a: number, b: number, c: number) => void;
+  readonly secure_message: (a: number, b: number, c: number, d: number) => void;
+  readonly restore_message: (a: number, b: number, c: number, d: number, e: number, f: number, g: number) => void;
   readonly encrypt_message: (a: number, b: number, c: number, d: number, e: number) => void;
-  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+  readonly decrypt_message: (a: number, b: number, c: number, d: number, e: number, f: number, g: number) => void;
   readonly __wbindgen_malloc: (a: number) => number;
   readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
+  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
   readonly __wbindgen_free: (a: number, b: number) => void;
+  readonly __wbindgen_exn_store: (a: number) => void;
 }
 
 export type SyncInitInput = BufferSource | WebAssembly.Module;
