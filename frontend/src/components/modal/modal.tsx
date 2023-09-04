a5785e1301a59d56ee9b1aefe98d3e879c2156b2
Mateusz Sikora <ms1qaz@gmail.com>
2023-09-04 21:45:06 +0200

    replace anchor with button to fix eslint problem

--- frontend/src/components/modal/modal.tsx
+++ frontend/src/components/modal/modal.tsx
@@ -52,12 +52,13 @@ export const Modal = ({ title, children, onCancel, onConfirm }: ModalProps) => {
             >
               Close
             </button>
-            <a
-              className="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800"
+            <button
+              type="button"
+              className="hs-dropdown-toggle py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800"
               onClick={onConfirm}
             >
               Save changes
-            </a>
+            </button>
           </div>
         </div>
       </div>
