9c2b392654d18da72b38f2293d1c030cc7dd16b3
Tomasz Drwięga <tomusdrw@gmail.com>
2023-09-29 11:47:30 +0200

    Navigation links.

--- frontend/src/components/navigation/navigation.tsx
+++ frontend/src/components/navigation/navigation.tsx
@@ -1,22 +1,36 @@
-import { Pane, majorScale, Button } from 'evergreen-ui';
+import { Pane, majorScale, Button, Link } from 'evergreen-ui';
 import logoWide from './logo256-wide-transparent.png';
+import { ReactNode } from 'react';
+import styles from './styles.module.scss';
+import { useIsActive } from '../../hooks/use-is-active';
+import clsx from 'clsx';
+
+const NavLink = ({ href, children }: { href: string; children: ReactNode }) => {
+  const isActive = useIsActive(href);
+  return (
+    <Link
+      className={clsx(styles.navLink, isActive ? styles.active : null)}
+      borderRadius={majorScale(1)}
+      margin={majorScale(1)}
+      href={href}
+      size="large"
+      padding={majorScale(2)}
+    >
+      {children}
+    </Link>
+  );
+};
 
 export const Navigation = () => {
   return (
     <Pane display="flex" padding={majorScale(3)} justifyContent="space-between" alignItems="center" height="195px">
       <a href="/">
-        <img src={logoWide} alt="ICOD logo" style={{ borderRadius: `${majorScale(3)}px` }} />
+        <img src={logoWide} alt="ICOD logo" style={{ borderRadius: `${majorScale(3)}px` }} width="256" height="118" />
       </a>
       <Pane>
-        <Button is="a" margin={8} href="/wizard" size="large">
-          Wizard
-        </Button>
-        <Button is="a" margin={8} href="/dev" size="large">
-          Devtools
-        </Button>
-        <Button is="a" margin={8} href="/login" size="large">
-          Login
-        </Button>
+        <NavLink href="/wizard">Wizard</NavLink>
+        <NavLink href="/dev">Devtools</NavLink>
+        <NavLink href="/login">Login</NavLink>
       </Pane>
     </Pane>
   );
---
+++ frontend/src/hooks/use-is-active.ts
@@ -0,0 +1,9 @@
+import { useCallback } from 'react';
+import { useLocation } from 'react-router-dom';
+
+export const useIsActive = (to: string) => {
+  const { pathname } = useLocation();
+  const isActive = pathname === to || (pathname.startsWith(to) && pathname.charAt(to.length) === '/');
+
+  return isActive;
+};
