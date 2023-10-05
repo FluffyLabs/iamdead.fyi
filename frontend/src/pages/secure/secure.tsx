33fcfc9cac6e5ad6a16b3eef6d0161e17be3865d
Tomasz Drwięga <tomusdrw@gmail.com>
2023-10-05 18:18:13 +0200

    Move Editor to it's own component file too.

--- frontend/src/pages/secure/secure.tsx
+++ frontend/src/pages/secure/secure.tsx
@@ -1,14 +1,13 @@
-import { Button, Group, Heading, Link, ListItem, majorScale, Pane, Text, UnorderedList } from 'evergreen-ui';
-import { useCallback, useState, MouseEvent, ReactNode } from 'react';
+import { Button, Group } from 'evergreen-ui';
+import { useCallback, useState, ReactNode } from 'react';
 
 import { Container } from '../../components/container';
 import { Navigation } from '../../components/navigation';
 import { ProgressBar } from '../../components/progress-bar';
-import { MessageEditor } from '../../components/message-editor';
-import { FUNNY } from './example-messages';
 import { OfflineWarning } from './components/offline-warning';
 import { ChunksConfigurationEditor } from './components/chunks-configuration-editor';
 import { SecureMessageResult } from './components/secure-message-result';
+import { Editor } from './components/editor';
 
 const useEditorState = () => {
   const [value, setValue] = useState('');
@@ -110,51 +109,4 @@ const Progress = ({ step, setStep }: ProgressProps) => {
       </Group>
     </>
   );
-};
-
-type EditorProps = {
-  value: string;
-  onChange: (a0: string, a1?: boolean) => void;
-  nextStep: () => void;
-};
-
-const Editor = ({ value, onChange, nextStep }: EditorProps) => {
-  return (
-    <>
-      <Heading marginTop={majorScale(3)}>What message would you like to encrypt?</Heading>
-      <Heading size={300} marginTop={majorScale(1)} marginBottom={majorScale(2)}>
-        Fell free to start with something basic for now, you will have a chance to edit it any time.
-      </Heading>
-      <Pane display="flex" padding={0}>
-        <Pane padding={0} margin={0} flex="6">
-          <MessageEditor value={value} onChange={onChange} />
-        </Pane>
-        <Pane margin="0" paddingTop="0" flex="2">
-          <Text>If you need help writing the message you may try some of the templates below.</Text>
-          <UnorderedList>
-            <ListItem>
-              <Link
-                href="#"
-                onClick={(e: MouseEvent<HTMLAnchorElement>) => {
-                  e.preventDefault();
-                  onChange(Examples.funny, true);
-                  return false;
-                }}
-              >
-                Funny Text
-              </Link>
-            </ListItem>
-          </UnorderedList>
-        </Pane>
-      </Pane>
-      <Pane display="flex" padding="0" justifyContent="center">
-        <Button appearance="primary" size="large" onClick={nextStep}>
-          Encrypt the message
-        </Button>
-      </Pane>
-    </>
-  );
-};
-const Examples = {
-  funny: FUNNY,
 };
