462e7dec3df4a542d980c52dcdaa098bd3a3b377
Tomasz Drwięga <tomusdrw@gmail.com>
2023-10-20 19:55:10 +0200

    Re-build the library.

--- crypto-js/pkg/icod_crypto_js_bg.wasm.d.ts
+++ crypto-js/pkg/icod_crypto_js_bg.wasm.d.ts
@@ -1,6 +1,8 @@
 /* tslint:disable */
 /* eslint-disable */
 export const memory: WebAssembly.Memory;
+export function identify(a: number, b: number, c: number): void;
+export function alter_chunks_name(a: number, b: number, c: number, d: number, e: number): void;
 export function __wbg_chunksconfiguration_free(a: number): void;
 export function __wbg_get_chunksconfiguration_required(a: number): number;
 export function __wbg_set_chunksconfiguration_required(a: number, b: number): void;
@@ -9,7 +11,6 @@ export function __wbg_set_chunksconfiguration_spare(a: number, b: number): void;
 export function chunksconfiguration_new(a: number, b: number): number;
 export function split_into_chunks(a: number, b: number, c: number, d: number): void;
 export function recover_key(a: number, b: number, c: number): void;
-export function identify(a: number, b: number, c: number): void;
 export function secure_message(a: number, b: number, c: number, d: number, e: number, f: number): void;
 export function restore_message(a: number, b: number, c: number, d: number, e: number): void;
 export function encrypt_message(a: number, b: number, c: number, d: number, e: number, f: number, g: number): void;
