9878248603f201e0bf4e3160089635b611563cc2
Tomasz Drwięga <tomusdrw@users.noreply.github.com>
2023-10-21 21:50:48 +0200

    Extract components of secure-message-result (#178)

---
+++ frontend/src/pages/secure/components/secure-message-result/chunk.tsx
@@ -0,0 +1,58 @@
+import {
+  Heading,
+  Pane,
+  majorScale,
+  Group,
+  Button,
+  HeatGridIcon,
+  KeyIcon,
+  DownloadIcon,
+  ManualIcon,
+} from 'evergreen-ui';
+import { Slab } from '../../../../components/slab';
+import { Chunk as ChunkT } from '../../../../services/crypto';
+
+type ChunkProps = {
+  chunk: ChunkT;
+  showDialog: (a0: number) => void;
+};
+
+export function Chunk({ chunk, showDialog }: ChunkProps) {
+  // TODO [ToDr] QR code value should rather be a link.
+  return (
+    <Slab
+      padding={0}
+      marginY={majorScale(5)}
+      title={chunk.raw}
+      display="flex"
+    >
+      <KeyIcon
+        size={majorScale(5)}
+        marginRight={majorScale(2)}
+      />
+      <Pane
+        display="flex"
+        flexDirection="column"
+      >
+        <Heading
+          marginRight={majorScale(1)}
+          marginBottom={majorScale(1)}
+          onClick={() => showDialog(chunk.chunkIndex)}
+          style={{ cursor: 'pointer' }}
+        >
+          {chunk.name}
+        </Heading>
+        <Group>
+          <Button
+            iconBefore={<HeatGridIcon />}
+            onClick={() => showDialog(chunk.chunkIndex)}
+          >
+            QR
+          </Button>
+          <Button iconBefore={<DownloadIcon />}>Download</Button>
+          <Button iconBefore={<ManualIcon />}>Certificate</Button>
+        </Group>
+      </Pane>
+    </Slab>
+  );
+}
---
+++ frontend/src/pages/secure/components/secure-message-result/chunks.tsx
@@ -0,0 +1,159 @@
+import { ChangeEvent, useCallback, useEffect, useState } from 'react';
+import {
+  Pane,
+  majorScale,
+  Group,
+  Button,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  Dialog,
+  DownloadIcon,
+  ManualIcon,
+  TextInputField,
+  TextareaField,
+} from 'evergreen-ui';
+import { ChunksMeta } from '../../../../components/piece-view';
+import { Chunk } from './chunk';
+import { QRWithClipboard } from '../../../../components/qr-with-clipboard';
+
+export function Chunks({
+  chunks,
+  onNameChange,
+  nameChangeError,
+  onDescriptionChange,
+}: {
+  chunks: ChunksMeta[];
+  onNameChange: (chunkIndex: number, name: string) => void;
+  nameChangeError?: string;
+  onDescriptionChange: (chunkIndex: number, description: string) => void;
+}) {
+  const [isShowingQrs, setIsShowingQrs] = useState(false);
+  const [activeChunkIdx, setActiveChunkIdx] = useState(0);
+  const showDialog = useCallback(
+    (id: number) => {
+      setActiveChunkIdx(id);
+      setIsShowingQrs(true);
+    },
+    [setActiveChunkIdx, setIsShowingQrs],
+  );
+
+  const chunk = chunks[activeChunkIdx];
+  const nextChunk = useCallback(() => {
+    setActiveChunkIdx((activeChunkIdx + 1) % chunks.length);
+  }, [activeChunkIdx, chunks]);
+  const prevChunk = useCallback(() => {
+    setActiveChunkIdx(activeChunkIdx === 0 ? chunks.length - 1 : activeChunkIdx - 1);
+  }, [activeChunkIdx, chunks]);
+
+  const [activeName, setActiveName] = useState(chunk.chunk.name);
+  // reset activeName when activeChunk changes
+  useEffect(() => {
+    setActiveName(chunk.chunk.name);
+  }, [chunk, setActiveName]);
+
+  const handleActiveNameChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      setActiveName(e.target.value);
+    },
+    [setActiveName],
+  );
+  const handleNameChange = useCallback(() => {
+    onNameChange(activeChunkIdx, activeName);
+  }, [activeName, activeChunkIdx, onNameChange]);
+  const handleDescriptionChange = useCallback(
+    (e: ChangeEvent<HTMLTextAreaElement>) => {
+      onDescriptionChange(activeChunkIdx, e.target.value);
+    },
+    [activeChunkIdx, onDescriptionChange],
+  );
+
+  return (
+    <>
+      {chunks.map((x: ChunksMeta) => (
+        <Chunk
+          key={x.chunk.chunkIndex}
+          chunk={x.chunk}
+          showDialog={showDialog}
+        />
+      ))}
+      {/* Not using footer, because it's not trivial to align buttons left/right */}
+      <Dialog
+        isShown={isShowingQrs}
+        title={chunk.chunk.name}
+        onCloseComplete={() => setIsShowingQrs(false)}
+        hasFooter={false}
+      >
+        {() => (
+          <Pane
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+          >
+            <Pane
+              display="flex"
+              flexDirection="row"
+              width="100%"
+            >
+              <Pane
+                flex="1"
+                display="flex"
+                alignItems="center"
+                flexDirection="column"
+                paddingX={majorScale(2)}
+              >
+                <QRWithClipboard value={chunk.chunk.raw.toUpperCase()} />
+                <Group marginTop={majorScale(2)}>
+                  <Button
+                    onClick={() => {}}
+                    iconBefore={<DownloadIcon />}
+                    marginBottom={majorScale(1)}
+                  >
+                    Download
+                  </Button>
+                  <Button iconBefore={<ManualIcon />}>Certificate</Button>
+                </Group>
+              </Pane>
+              <Pane flex="1">
+                <TextInputField
+                  label="Piece name"
+                  value={activeName}
+                  onChange={handleActiveNameChange}
+                  onBlur={handleNameChange}
+                  isInvalid={!!nameChangeError}
+                  validationMessage={nameChangeError}
+                  autoFocus
+                />
+                <TextareaField
+                  label="Extended description"
+                  hint="The description is only visible in the certificate."
+                  value={chunk.description}
+                  onChange={handleDescriptionChange}
+                />
+              </Pane>
+            </Pane>
+            <Pane
+              width="100%"
+              display="flex"
+              justifyContent="space-between"
+              marginY={majorScale(3)}
+            >
+              <Button
+                onClick={prevChunk}
+                iconBefore={<ChevronLeftIcon />}
+              >
+                Prev
+              </Button>
+              <Button
+                appearance="primary"
+                onClick={nextChunk}
+                iconAfter={<ChevronRightIcon />}
+              >
+                Next
+              </Button>
+            </Pane>
+          </Pane>
+        )}
+      </Dialog>
+    </>
+  );
+}
