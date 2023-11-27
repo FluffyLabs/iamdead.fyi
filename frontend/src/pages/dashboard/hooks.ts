cfb73c149904878abc9e950c7e1a5956f0adf665
Tomasz Drwięga <tomusdrw@gmail.com>
2023-11-27 16:22:28 +0100

    Update react-query

--- frontend/src/pages/dashboard/hooks.ts
+++ frontend/src/pages/dashboard/hooks.ts
@@ -2,7 +2,10 @@ import { useQuery } from '@tanstack/react-query';
 import { axios } from '../../services/axios';
 
 export function useUser() {
-  const { isLoading, error, data, isSuccess } = useQuery([], ({ signal }) => axios.get('/api/users/me', { signal }));
+  const { isLoading, error, data, isSuccess } = useQuery({
+    queryKey: [],
+    queryFn: ({ signal }) => axios.get('/api/users/me', { signal }),
+  });
 
   return {
     me: data?.data,
