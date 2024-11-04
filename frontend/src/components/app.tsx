be048e931905eaaab917cc20e8351b15b804b9ef
Tomasz Drwięga <tomusdrw@gmail.com>
2024-11-04 18:24:32 +0100

    Hash router.

--- frontend/src/components/app.tsx
+++ frontend/src/components/app.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { Routes } from '../routes';
@@ -9,11 +9,11 @@ const queryClient = new QueryClient();
 export function App() {
   return (
     <EvergreenTheme>
-      <BrowserRouter>
+      <HashRouter>
         <QueryClientProvider client={queryClient}>
           <Routes />
         </QueryClientProvider>
-      </BrowserRouter>
+      </HashRouter>
     </EvergreenTheme>
   );
 }
